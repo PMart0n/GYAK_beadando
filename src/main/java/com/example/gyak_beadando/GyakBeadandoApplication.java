176be487076649584c480aac957b941e5847fc63
PMart0n <pataki1996@gmail.com>
2025-11-16 10:57:57 +0100

    projekt vaz es config

---
+++ src/main/java/com/example/gyak_beadando/GyakBeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gyak_beadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyakBeadandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyakBeadandoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gyak_beadando/GyakBeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyak_beadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyakBeadandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
