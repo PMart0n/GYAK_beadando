273aff1a3d1cfe90732d06b5cd6777cd8be4cac0
PMart0n <pataki1996@gmail.com>
2025-11-16 15:01:17 +0100

    KESZ VAZ: Adatbazis oldal elerheto, Security beallitva, adatok listazasa mukodik

--- src/main/java/com/example/gyak_beadando/config/WebSecurityConfig.java
+++ src/main/java/com/example/gyak_beadando/config/WebSecurityConfig.java
@@ -14,8 +14,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/assets/**", "/images/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/", "/home", "/adatbazis", "/assets/**", "/images/**").permitAll()                        .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                         .permitAll()
--- src/main/java/com/example/gyak_beadando/controller/HomeController.java
+++ src/main/java/com/example/gyak_beadando/controller/HomeController.java
@@ -10,17 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Repository lekeres
     @Autowired private FilmRepository filmRepo;
     @Autowired private MoziRepository moziRepo;
 
-    // lefutn http://localhost:8000 oldal
+    // 1. Főoldal: Csak a bemutatkozás
     @GetMapping("/")
-    public String fooldal(Model model) {
+    public String fooldal() {
+        return "index";
+    }
 
+    // 2. Adatbázis oldal: adatok lekérése a táblázathoz
+    @GetMapping("/adatbazis")
+    public String adatbazisListazas(Model model) {
         model.addAttribute("filmek", filmRepo.findAll());
         model.addAttribute("mozik", moziRepo.findAll());
 
-        return "index";
+        return "adatbazis"; //új adatbazis.html fájl
     }
 }
