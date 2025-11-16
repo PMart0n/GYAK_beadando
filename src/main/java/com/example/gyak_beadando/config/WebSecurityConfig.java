deb589218e1f7e76afd365f76a53bc4de7b0c945
PMart0n <pataki1996@gmail.com>
2025-11-16 14:15:02 +0100

    Spectral design integralasa es Security fix a statikus fajlokhoz

--- src/main/java/com/example/gyak_beadando/config/WebSecurityConfig.java
+++ src/main/java/com/example/gyak_beadando/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll() // A főoldal mindenkinek elérhető
+                        .requestMatchers("/", "/home", "/assets/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
